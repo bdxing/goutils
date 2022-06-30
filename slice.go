de639bf57ce6d5137c5c067a4acf0e2127cfa7f5
bdxing <353150280@qq.com>
2022-06-30 10:31:48 +0800

    feat(slice): del InSlice()

--- slice.go
+++ slice.go
@@ -1,57 +1,5 @@
 package goutils
 
-// InSlice array 里是否存在 element。
-// 注意：只支持的 Slice 基础类型，其他类型直接返回 false
-func InSlice(element, array interface{}) bool {
-	switch array.(type) {
-	case []int:
-		if e, s := element.(int); s {
-			return InSliceInt(e, array.([]int))
-		}
-	case []uint:
-		if e, s := element.(uint); s {
-			return InSliceUint(e, array.([]uint))
-		}
-	case []int8:
-		if e, s := element.(int8); s {
-			return InSliceInt8(e, array.([]int8))
-		}
-	case []uint8:
-		if e, s := element.(uint8); s {
-			return InSliceUint8(e, array.([]uint8))
-		}
-	case []int32:
-		if e, s := element.(int32); s {
-			return InSliceInt32(e, array.([]int32))
-		}
-	case []uint32:
-		if e, s := element.(uint32); s {
-			return InSliceUint32(e, array.([]uint32))
-		}
-	case []int64:
-		if e, s := element.(int64); s {
-			return InSliceInt64(e, array.([]int64))
-		}
-	case []uint64:
-		if e, s := element.(uint64); s {
-			return InSliceUint64(e, array.([]uint64))
-		}
-	case []float32:
-		if e, s := element.(float32); s {
-			return InSliceFloat32(e, array.([]float32))
-		}
-	case []float64:
-		if e, s := element.(float64); s {
-			return InSliceFloat64(e, array.([]float64))
-		}
-	case []string:
-		if e, s := element.(string); s {
-			return InSliceStr(e, array.([]string))
-		}
-	}
-	return false
-}
-
 // InSliceInt array 里是否存在 element
 func InSliceInt(element int, array []int) bool {
 	for k := range array {
